db18157869e4d8c069a550d35b128411bee0baba
gmtev <gmtev1007@gmail.com>
2024-03-09 17:28:17 +0200

    Create polymorphism_and_abstraction.py

---
+++ Exercises/polymorphism_and_abstraction.py
@@ -0,0 +1,105 @@
+# robots
+
+class Robot:
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def sensors_amount():
+        return 1
+
+
+class MedicalRobot(Robot):
+    @staticmethod
+    def sensors_amount():
+        return 6
+
+
+class ChefRobot(Robot):
+    @staticmethod
+    def sensors_amount():
+        return 4
+
+
+class WarRobot(Robot):
+    @staticmethod
+    def sensors_amount():
+        return 12
+
+
+def number_of_robot_sensors(robot):
+    print(robot.sensors_amount())
+
+
+# image area
+class ImageArea:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def __lt__(self, other):
+        return self.get_area() < other.get_area()
+
+    def __le__(self, other):
+        return self.get_area() <= other.get_area()
+
+    def __eq__(self, other):
+        return self.get_area() == other.get_area()
+
+
+# playing
+def start_playing(obj):
+    return obj.play()
+
+
+class Guitar:
+    def play(self):
+        return "Playing the guitar"
+
+
+class Children:
+    def play(self):
+        return "Children are playing"
+
+
+# shapes
+
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+    @abstractmethod
+    def calculate_perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.__radius = radius
+
+    def calculate_area(self):
+        return pi * self.__radius ** 2
+
+    def calculate_perimeter(self):
+        return self.__radius * 2 * pi
+
+
+class Rectangle(Shape):
+    def __init__(self, height, width):
+        self.__height = height
+        self.__width = width
+
+    def calculate_area(self):
+        return self.__width * self.__height
+
+    def calculate_perimeter(self):
+        return 2 * (self.__height + self.__width)
